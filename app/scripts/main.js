d00bad894543f9039d2f18be011e840ae13e31d7
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-02-05 17:04:58 -0500

    Just getting framework started.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,25 @@
 console.log('\'Allo \'Allo!');
 
 function Collection (models) {
-	this.models = models;
+	this.models = models || [];
+	// this.find = function(id) {
+	// 	return this.models[id-1]	 
+	this.find = function(id) {
+		this.models.forEach(function(value, index)
+			var result 
+			if typeof(id) != 'string') {
+				throw new Error("Whoops!");
+		}
+
+		this.models.forEach(function(value, index) {
+			if (value.id == id) {
+				var result = value;
+			}
+		)};
+
+		if (result) {
+			return result;
+		}
+	}
 }
+
--- test/spec/test.js
+++ test/spec/test.js
@@ -7,6 +7,7 @@
         describe(',when run', function () {
             it('should return a new object', function () {
               var students = new Collection;
+
               expect(students.constructor.name).to.equal("Collection");
             });
 
@@ -17,9 +18,34 @@
             	expect(students.models).to.be.a('Array');
             	expect(students.models[1].name).to.equal('Jack');
             });
+        });
+    });
 
 // Write more tests below... 
+    describe('A Collection instance', function () {
+        describe('has a .find() menthod', function () {
+            it('should return an object when given an ide that is present in the models', function(){
+            	var toBeAdded = [{name: 'Bower', id: '1'}, {name: 'Jack', id:'2'}]
+            	var students = new Collection(toBeAdded)
+
+            	expect(students.find('1')).to.exist;
+            	expect(students.find('1')).to.be.an('object');
+            	expect(students.find('1')).to.include.key('name');
+            	expect(students.find('1')).to.deep.equal({name: 'Bower', id: '1'});
+            	expect(students.find('1')).to.deep.property('name', 'Bower');
+            	// expect(students.find('1')).to.equal({name: 'Bower', id: '1'});
+            });
+            it('should not return an object when that id is not present in the models');
+            
+            it('should return undefined when that id is not present in the models');
+            it('should throw an error when given an argument other than a string');
+        });
+       
+        describe("has an .add method", function(){
 
         });
     });
 })();
+
+
+
