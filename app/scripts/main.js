2fbfdd5449dd11467aaff8a8a99af97c0a64c047
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-02-05 23:12:30 -0500

    Added tests for .add

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,24 +2,29 @@ console.log('\'Allo \'Allo!');
 
 function Collection (models) {
 	this.models = models || [];
-	// this.find = function(id) {
-	// 	return this.models[id-1]	 
+
 	this.find = function(id) {
-		this.models.forEach(function(value, index)
-			var result 
-			if typeof(id) != 'string') {
-				throw new Error("Whoops!");
+		var result;
+
+		if (typeof(id) != 'string') {
+			throw new Error("Whoops!");
 		}
 
-		this.models.forEach(function(value, index) {
+		this.models.forEach(function(value, index){
 			if (value.id == id) {
-				var result = value;
+				result = value;
 			}
-		)};
+		});
 
 		if (result) {
 			return result;
 		}
+	}
+
+	this.add = function(name, id){
+
+		if (typeof(name) != 'string' || typeof(id) != 'string')
+		this.models.push({name: name, id: id})
 	}
 }
 
--- test/spec/test.js
+++ test/spec/test.js
@@ -4,48 +4,83 @@
     'use strict';
 
     describe('A Collection constructor', function () {
-        describe(',when run', function () {
+        describe(', when run', function () {
             it('should return a new object', function () {
               var students = new Collection;
-
+ 
               expect(students.constructor.name).to.equal("Collection");
             });
-
-            it('store its first param in a property called models', function(){
-            	var toBeAdded = [{name: 'Bower', id: '1'}, {name: 'Jack', id:'2'}]
-            	var students = new Collection(toBeAdded)
-
-            	expect(students.models).to.be.a('Array');
-            	expect(students.models[1].name).to.equal('Jack');
+ 
+            it('stores its first param in a property called models', function(){
+              var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id:'2'}]
+              var students = new Collection(toBeAdded)
+              
+              expect(students.models).to.be.a('Array');
+              expect(students.models[1].name).to.equal('Jack');
             });
+ 
         });
     });
-
 // Write more tests below... 
     describe('A Collection instance', function () {
-        describe('has a .find() menthod', function () {
-            it('should return an object when given an ide that is present in the models', function(){
-            	var toBeAdded = [{name: 'Bower', id: '1'}, {name: 'Jack', id:'2'}]
-            	var students = new Collection(toBeAdded)
-
-            	expect(students.find('1')).to.exist;
-            	expect(students.find('1')).to.be.an('object');
-            	expect(students.find('1')).to.include.key('name');
-            	expect(students.find('1')).to.deep.equal({name: 'Bower', id: '1'});
-            	expect(students.find('1')).to.deep.property('name', 'Bower');
-            	// expect(students.find('1')).to.equal({name: 'Bower', id: '1'});
-            });
-            it('should not return an object when that id is not present in the models');
-            
-            it('should return undefined when that id is not present in the models');
-            it('should throw an error when given an argument other than a string');
+      describe('has a .find() menthod', function () {
+        it('should return an object when given an id that is present in the models', function(){
+          var students = new Collection([{name: 'Jim', id: '99'}]);
+          expect(students.find('99')).to.deep.equal({name: 'Jim', id: '99'});
         });
-       
-        describe("has an .add method", function(){
 
+        it("should not return an object when that id is not present in the models", function(){
+          var students = new Collection([{name: 'Jim', id: '99'}]);
+          expect(students.find('1')).to.not.equal({name: 'Jim', id: '99'});
         });
+ 
+        it("should return undefined when that id is not present in the models", function(){
+          var students = new Collection([{name: 'Jim', id: '99'}]);
+          expect(students.find('1')).to.equal(undefined);
+        });
+ 
+        it("should throw an error when given an arguemnt other than a string", function(){
+          var students = new Collection([{name: 'Jim', id: '99'}]);
+          expect(function(){students.find(1)}).to.throw(Error);
+          expect(function(){students.find({})}).to.throw(Error);
+          expect(function(){students.find([])}).to.throw(Error);
+        });      
+      }); 
+            // .add Method
+      describe("has an .add method", function(){
+				it("should add the object it's given to the models property", function(){
+					var fallStudents = [{name: 'Eddie', id: '5'},{name: 'Grandpa', id:'6'}];
+          var students = new Collection(fallStudents);
+          students.add('Herman', '7')
+
+          expect(fallStudents[2]).to.deep.equal({name: 'Herman', id: '7'});
+				});
+
+        it("should increase the models property's length by 1", function(){
+					var fallStudents = [{name: 'Eddie', id: '5'},{name: 'Grandpa', id:'6'}];
+          var students = new Collection(fallStudents);
+          students.add('Marylin', '8');
+
+          expect(fallStudents.length).to.equal(3);
+        });
+
+        it("should not  accept an empty object as an argument", function(){
+        		expect(students.add({})).to.throw(Error);
+        });
+        it("should throw an error when given an object without and id property", function(){
+        		expect(students.add({name: 'Lily'})).to.throw(Error);
+        });
+
+      });
+
+      describe("has a .remove method", function(){
+				it("should, when given an id, remove the corresponding object from the models property")
+        it("should decrease the models property's length by 1");
+        it("should only accept a single string as an id argument");
+        it("should return true on successful removal");
+      });
     });
 })();
 
-
+				
 
